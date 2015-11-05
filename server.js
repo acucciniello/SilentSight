db9370463eb31a909fef95eae0cd7dbb01b35d70
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2015-11-05 09:22:46 -0500

    database changes

--- server.js
+++ server.js
@@ -5,28 +5,16 @@ var got = require('got');
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 var mongoose = require('mongoose');
-var url = 'mongodb://localhost:3000/silentSight';
+var url = 'mongodb://localhost:27017/silentSight';
 var order_book;
-var dataOrderBook;
+var inBids;
+var inAsks;
+
 var testBook;
 server.listen(3000);
 
 
-mongoose.connect(url);
 //mongoose.model('silentSight');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(callback){
-    dataOrderBook = mongoose.Schema({
-        order_info: {
-            bids: [Number],
-            asks: [Number]
-        }
-    })
-var silentSight = mongoose.model('silentSight', dataOrderBook);
-testBook = new silentSight;
-});
-
 
 
 app.get('/', function(req, res){
@@ -37,22 +25,41 @@ app.get('/', function(req, res){
 
 got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
 	    order_book = data;
-        console.log(order_book);
+        //console.log(order_book);
 })
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback){
+    //Create bids schema
+    var bids = mongoose.Schema({
+        price: Number,
+        amount: Number
+    });
+    //Create asks schema
+    var asks = mongoose.Schema({
+        price: Number,
+        amount: Number
+    })
 
+    var givenBids = mongoose.model('givenBids', bids);
+    inBids = new givenBids;
+
+    var givenAsks = mongoose.model('givenAsks', asks);
+    inAsks = new givenAsks;
+});
 
 io.on('connection', function (socket) {
 	pullData(socket);
 	socket.on('dataOrderBook', function(order_book) {
-		console.log(order_book);
+		//console.log(order_book);
 	});
 });
 
+mongoose.connect(url);
 
-
-function pullData (socket) {
-    repeat(1500, function(){
+function pullData (socket, inBids, inAsks) {
+    repeat(1500, function(inBids, inAsks){
     	console.log(Date());
     	got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
     		console.log(Date());
@@ -63,9 +70,15 @@ function pullData (socket) {
             order_holder.bids = order_bids;
             order_holder.asks = order_asks;
         	socket.emit('dataOrderBook',  order_holder);    
-            //Atempting to store the data in the database's schema
-            testBook.order_info.bids = order_bids;
-            testBook.order_info.asks = order_asks;
         });
+            inBids = order_book.bids;
+            inBids.save();
+            inAsks = order_book.asks;
+            inAsks.save();
+            //Atempting to store the data in the database's schema
+            //testBook.bids = order_bids;
+            //testBook.asks = order_asks;
+            console.log(inBids);
+            console.log(inAsks);
     });
 }
