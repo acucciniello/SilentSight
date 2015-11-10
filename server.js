f66aed8c58d9d7ca3e70adc0d08e22b7cc8a4ba8
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2015-11-10 09:36:32 -0500

    Closer to db functionality

--- server.js
+++ server.js
@@ -5,54 +5,51 @@ var got = require('got');
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 var mongoose = require('mongoose');
-var url = 'mongodb://localhost:3000/silentSight';
+var url = 'mongodb://localhost:27017/silentSight';
 var order_book;
 var dataOrderBook;
 var testBook;
 server.listen(3000);
 
-
+//Database work
 mongoose.connect(url);
-//mongoose.model('silentSight');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback){
+    var transaction = mongoose.Schema({
+        price:Number,
+        amount: Number,
+        type: String
+    });
     dataOrderBook = mongoose.Schema({
-        order_info: {
-            bids: [Number],
-            asks: [Number]
-        }
-    })
+        transactions: [transaction],
+        timestamp: Number
+    });
 var silentSight = mongoose.model('silentSight', dataOrderBook);
 testBook = new silentSight;
 });
-
-
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 })
 
-
-
+//Pull Data from Bit Stamp
 got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
 	    order_book = data;
-        console.log(order_book);
+        //console.log(order_book);
 })
 
-
-
+//Connect to Client
 io.on('connection', function (socket) {
-	pullData(socket);
+	pullData(socket, testBook);
 	socket.on('dataOrderBook', function(order_book) {
-		console.log(order_book);
+		//console.log(order_book);
 	});
 });
 
-
-
-function pullData (socket) {
-    repeat(1500, function(){
+//Send Table to Client every 1.5 seconds
+function pullData (socket, testBook) {
+    repeat(15000, function(testBook){
     	console.log(Date());
     	got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
     		console.log(Date());
@@ -63,9 +60,8 @@ function pullData (socket) {
             order_holder.bids = order_bids;
             order_holder.asks = order_asks;
         	socket.emit('dataOrderBook',  order_holder);    
-            //Atempting to store the data in the database's schema
-            testBook.order_info.bids = order_bids;
-            testBook.order_info.asks = order_asks;
+            testBook = order_book;
+            console.log(testBook);
         });
     });
 }
