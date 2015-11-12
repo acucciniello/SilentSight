da4ebdb0d99dfb537d78442e362aad90f85a484a
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2015-11-12 09:12:18 -0500

    Updated branch with save function, but it still does not seem to save properly.  Am I doing the save in the wrong place?

--- server.js
+++ server.js
@@ -1,82 +1,80 @@
 require('waitjs');
 var express = require('express')
-var	app = express()
+var app = express()
 var got = require('got');
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 var mongoose = require('mongoose');
 var url = 'mongodb://localhost:27017/silentSight';
 var order_book;
-var inBids;
-var inAsks;
-
+var dataOrderBook;
 var testBook;
 server.listen(3000);
 
-
-//mongoose.model('silentSight');
-
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-})
-
-
-
-got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
-	    order_book = data;
-        //console.log(order_book);
-})
-
+//Database work
+mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback){
-    //Create bids schema
-    var bids = mongoose.Schema({
-        price: Number,
-        amount: Number
+    var transaction = mongoose.Schema({
+        price:Number,
+        amount: Number,
+        type: String
     });
-    //Create asks schema
-    var asks = mongoose.Schema({
-        price: Number,
-        amount: Number
-    })
-
-    var givenBids = mongoose.model('givenBids', bids);
-    inBids = new givenBids;
-
-    var givenAsks = mongoose.model('givenAsks', asks);
-    inAsks = new givenAsks;
+    dataOrderBook = mongoose.Schema({
+        transactions: [transaction],
+        timestamp: Number
+    });
+var silentSight = mongoose.model('silentSight', dataOrderBook);
+testBook = new silentSight;
+testBook.save(function(err){
+    if (err){
+       return console.error(err); 
+    } 
+    else {
+        console.log("testBook has been Saved");
+    }
+    });
 });
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+//Pull Data from Bit Stamp
+got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
+        order_book = data;
+        //console.log(order_book);
+})
+
+//Connect to Client
 io.on('connection', function (socket) {
-	pullData(socket);
-	socket.on('dataOrderBook', function(order_book) {
-		//console.log(order_book);
-	});
+    pullData(socket);
+    socket.on('dataOrderBook', function(order_book) {
+        //console.log(order_book);
+    });
 });
 
-mongoose.connect(url);
-
-function pullData (socket, inBids, inAsks) {
-    repeat(1500, function(inBids, inAsks){
-    	console.log(Date());
-    	got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
-    		console.log(Date());
-        	order_book = JSON.parse(data);
+//Send Table to Client every 1.5 seconds
+function pullData (socket) {
+    repeat(1500, function(){
+        console.log(Date());
+        got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
+            console.log(Date());
+            order_book = JSON.parse(data);
             var order_bids = order_book.bids;
             var order_asks = order_book.asks;
             var order_holder = {}
             order_holder.bids = order_bids;
             order_holder.asks = order_asks;
-        	socket.emit('dataOrderBook',  order_holder);    
+            socket.emit('dataOrderBook',  order_holder);    
+            testBook = order_book;
+            console.log(testBook);
+            //testBook.save();
         });
-            inBids = order_book.bids;
-            inBids.save();
-            inAsks = order_book.asks;
-            inAsks.save();
-            //Atempting to store the data in the database's schema
-            //testBook.bids = order_bids;
+    });
+}
+s = order_bids;
             //testBook.asks = order_asks;
             console.log(inBids);
             console.log(inAsks);
