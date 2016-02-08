87a1570292d919b9fc2039008a0caa019cd38c59
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2016-02-08 18:38:29 -0500

    Added npde-pg module to Insert data into databsde and created a function to input the time in the database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('waitjs');
+
 var express = require('express')
 var app = express()
 var got = require('got');
@@ -6,26 +7,16 @@ var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 var pg = require('pg');
 var order_book;
+var currentTime;
 var dataOrderBook;
-var conString  = "pg://username:password@localhost/orderbook";
+var conString  = "pg://Antonio:timleetimlee@localhost/orderbook";
+var format = require('pg-format');
+var price;
+var amount;
+
+
 
 server.listen(3000);
-
-//DATABASE 
-pg.connect(conString, function(err, client, done){
-    console.log("Connected to at least something");
-    if(err){
-        return console.error('error fetching client from pool', err);
-    }
-    client.query('INSERT INTO orderdata VALUES (200.0, 2.000000, $$bids$$, $$2001-12-12$$)', function(err, result){
-        //call 'done()' to realease the client back to the pool
-        done();
-        if(err){
-            return console.error('error running query', err);
-        }
-        console.log("We added this tp the table");
-        });
-    });
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -33,9 +24,11 @@ app.get('/', function(req, res){
 
 //Pull Data from Bit Stamp
 got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
-	 order_book = data;
+     order_book = data;
          //console.log(order_book);
 })
+
+
 
 //Connect to Client
 io.on('connection', function (socket) {
@@ -47,17 +40,78 @@ io.on('connection', function (socket) {
 
 //Send Table to Client every 1.5 seconds
 function pullData (socket) {
-    repeat(15000, function(){
-        console.log(Date());
+    repeat(1500, function(){
         got('https://www.bitstamp.net/api/order_book/', function(error, data, res) {
-            console.log(Date());
+            currentTime = getCurrentTime(currentTime);
             order_book = JSON.parse(data);
             var order_bids = order_book.bids;
             var order_asks = order_book.asks;
+            pg.connect(conString, function(err, client, done){
+                console.log("Connected to at least something");
+                if(err){
+                    return console.error('error fetching client from pool', err);
+                }
+                for (var i = 0; i < 20; i++){
+                    askingPrice = order_asks[i][0];
+                    askingAmount = order_asks[i][1];
+                    biddingPrice = order_bids[i][0];
+                    biddingAmount = order_bids[i][1];
+                    console.log(price);
+                    console.log(amount);
+                    var bidString = format('INSERT INTO orderdata VALUES(%L, %L, $$bids$$ , %L)', biddingPrice, biddingAmount, currentTime);
+                    var askString = format('INSERT INTO orderdata VALUES(%L, %L, $$asks$$ , %L)', askingPrice, askingAmount, currentTime);
+                    //DATABASE 
+                    client.query(bidString, function(err, result){
+                        //call 'done()' to realease the client back to the pool
+                        done();
+                        if(err){
+                            return console.error('error running query', err);
+                        }
+                        console.log("We added a bid to the table");
+                    });
+                    client.query(askString, function(err, result){
+                        done();
+                        if(err){
+                            return console.error('error running query', err);
+                        }
+                        console.log("We added a ask to the table");
+                    });
+                }
+                });
+                
+
             var order_holder = {}
             order_holder.bids = order_bids;
             order_holder.asks = order_asks;
             socket.emit('dataOrderBook',  order_holder);   
         });
     });
+};
+
+function getCurrentTime(i){
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+
+    h = h.toString();
+    m = m.toString();
+    s = s.toString();
+
+    if (h < 10)
+    {
+        h = '0'+h;
+    }
+    if (m < 10)
+    {
+        m = '0'+m;
+    }
+    if (s < 10)
+    {
+        s = '0'+s;
+    }
+    i = h + ':' + m + ':' + s;
+
+    i = i.toString();
+    return i;
 }
